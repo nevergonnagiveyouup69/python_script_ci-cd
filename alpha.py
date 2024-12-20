7d76930e50da911113dc8eb18b08798b2a727d53
Kaushal <kaushal.r@7edge.com>
2024-12-20 10:19:16 +0530

    WCH-210 #time 1m feature: using google search

--- alpha.py
+++ alpha.py
@@ -16,7 +16,7 @@ def get_company_url(company_name):
                         response = model.generate_content("I want you to analyze the data and provide a financial analysis (profitability) of this in single paragraph no highlights fundamentals"+str(financial_data))
                         generated_text = response.text
                         
-                        
+                        genai.configure(api_key=os.getenv('API_KEY_GEN_3'))
                         model2 = genai.GenerativeModel("gemini-1.5-flash")
                         response2 = model2.generate_content("Summarize the profitability of this investment in one word: 'Low,' 'Moderate,' 'High,' 'Very High,' or 'None'.fundamentals"+str(financial_data))
                         profits = response2.text

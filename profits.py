4f1846ac769cbd090179a63c4b874b502ddb116a
Kaushal <kaushal.r@7edge.com>
2024-12-20 16:13:43 +0530

    WCH-210 #time 1m feature: using google search

--- profits.py
+++ profits.py
@@ -2,7 +2,7 @@ import google.generativeai as genai # type: ignore
 import os
 
 def profits_chance(financial_data):
-        genai.configure(api_key=os.getenv('API_KEY_GEN_3'))
+        genai.configure(api_key=os.getenv('API_KEY_GEN'))
         model = genai.GenerativeModel("gemini-1.5-flash")
         response2 = model.generate_content("Summarize the profitability of this investment in one word: 'Low,' 'Moderate,' 'High,' 'Very High,' or 'None'.fundamentals:"+str(financial_data))
         profits = response2.text
